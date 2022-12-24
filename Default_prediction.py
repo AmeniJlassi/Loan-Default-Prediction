396efeaf10c799bc621826c7161719f61e025718
Ameni Jlassi <ameni.jlassi43@gmail.com>
2022-12-25 00:12:11 +0100

    Update Default_prediction.py

--- Default_prediction.py
+++ Default_prediction.py
@@ -17,3 +17,210 @@ from sklearn import linear_model,svm
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import precision_recall_curve
 
+Final_data = pd.read_csv("german_credit_data.csv", index_col=0)
+Final_data.head()
+Final_data.info()
+
+#Filling Missing values and Feature scaling
+#some features have some missing values. We filled those missing those values with the mean of the column
+Final_data.fillna(Final_data.mean(),inplace = True)
+HTML(Final_data.tail().to_html())
+print("Current shape of dataset :",Final_data.shape)
+
+#We scaled the features all the features here using standard scaler
+scl = preprocessing.StandardScaler() #instance of preprocessing
+fields = Final_data.columns.values[:-1]
+data_clean = pd.DataFrame(scl.fit_transform(Final_data[fields]), columns = fields)
+data_clean['Risk'] = Final_data['Risk']
+data_clean['Risk'].value_counts()
+
+loanstatus_0 = data_clean[data_clean["Risk"]==0]
+loanstatus_1 = data_clean[data_clean["Risk"]==1]
+subset_of_loanstatus_0 = loanstatus_0.sample(n=200)
+subset_of_loanstatus_1 = loanstatus_1.sample(n=200)
+data_clean = pd.concat([subset_of_loanstatus_1, subset_of_loanstatus_0])
+data_clean = data_clean.sample(frac=1).reset_index(drop=True)
+print("Current shape of dataset :",data_clean.shape)
+data_clean.head()
+
+#correlation values between the features
+data_clean.corr()
+
+#Models
+#Random Forest with randomized Search
+from sklearn.grid_search import RandomizedSearchCV
+rf = RandomForestClassifier(criterion='gini', random_state=0)
+maxFeatures = range(1,data_clean.shape[1]-1)
+param_dist = dict(max_features=maxFeatures)
+rand = RandomizedSearchCV(rf, param_dist, cv=10, scoring='accuracy', n_iter=len(maxFeatures), random_state=10)
+X=data_clean.iloc[:,:-1].values
+y=data_clean.iloc[:,-1].values
+rand.fit(X,y)
+mean_scores = [result.mean_validation_score for result in rand.grid_scores_]
+#print('Best Accuracy = '+str(rand.best_score_))
+print(rand.best_estimator_)
+
+#We used the randomized search to find the best hyper paramters for the model.
+randomForest = RandomForestClassifier(bootstrap=True,criterion = "gini",max_features=rand.best_estimator_.max_features,random_state=0 )
+randomForest.fit(X_train,y_train)
+rfPredict = randomForest.predict(X_test)
+rfPredictproba = randomForest.predict_proba(X_test)[:,1] #for ROC curve
+rfAccuracy = accuracy_score(y_test,rfPredict)
+roc_score = metrics.roc_auc_score(y_test,rfPredict)
+print(rfAccuracy)
+
+#Feature importance with Random Forest
+fig, ax = plt.subplots()
+width=0.35
+ax.bar(np.arange(len(features)-1), randomForest.feature_importances_, width, color='r')
+ax.set_xticks(np.arange(len(randomForest.feature_importances_)))
+ax.set_xticklabels(X_train.columns.values,rotation=90)
+plt.title('Feature Importance')
+ax.set_ylabel('Normalized Gini Importance')
+
+#ROC curve of Random Forest classifier
+plotAUC(y_test,rfPredictproba, 'Random Forest')
+plt.show()
+plt.figure(figsize=(6,6))
+#confusion matrix
+plot_confusion_matrix(rfPredict, normalize=True)
+plt.show()
+
+#Logistic Regression with Grid Search
+from sklearn.model_selection import GridSearchCV
+def cross_validation_best_parameters(model, param_grid):
+    grid = GridSearchCV(model, param_grid,cv=10, scoring='accuracy')
+    X=data_clean.iloc[:,:-1].values
+    y=data_clean.iloc[:,-1].values
+    grid.fit(X,y)
+    mean_scores = [result.mean_validation_score for result in grid.grid_scores_]
+    return mean_scores,grid.best_score_,grid.best_estimator_
+logreg = linear_model.LogisticRegression(random_state=0)
+c=[0.001, 0.01, 0.1, 1, 10, 100, 1000]
+param_grid = dict(C=c)
+mean_scores,Best_Accuracy, Best_classifier = cross_validation_best_parameters(logreg,param_grid)
+print("Best accuracy is "+ str(Best_Accuracy))
+print(Best_classifier)
+
+clf_LR = linear_model.LogisticRegression(C=Best_classifier.C)
+clf_LR.fit(X_train,y_train)
+LR_Predict = clf_LR.predict_proba(X_test)[:,1]
+LR_Predict_bin = clf_LR.predict(X_test)
+LR_Accuracy = accuracy_score(y_test,LR_Predict.round())
+print("Logistic regression accuracy is ",LR_Accuracy)
+plotAUC(y_test,rfPredictproba, 'Random Forest')
+plotAUC(y_test,LR_Predict,'Logistic Regression')
+plt.show()
+plt.figure(figsize=(6,6))
+plot_confusion_matrix(LR_Predict_bin, normalize=True)
+plt.show()
+
+#Support Vector Machines(SVM) with Grid Search CV
+from sklearn.grid_search import GridSearchCV    ## takes too much time to run this cell.
+clf_svm = svm.SVC()
+powers = range(0,5)
+cs = [10**i for i in powers]
+param_grid = dict(C=cs)
+grid = GridSearchCV(clf_svm, param_grid, cv=10, scoring='accuracy')
+grid.fit(data_clean.iloc[:,:-1].values, data_clean.iloc[:,-1].values)
+grid_mean_scores = [result.mean_validation_score for result in grid.grid_scores_]# create a list of the mean scores only
+print(grid.best_params_)
+print("---------------")
+print(grid.best_estimator_)
+#accuracy, ROC curve and Confusion matrix of SVM
+clf_svm = svm.SVC(kernel = "rbf", C=grid.best_estimator_.C)
+clf_svm.fit(X_train.iloc[:,:],y_train)
+predictions_svm = clf_svm.predict(X_test.iloc[:,:])
+predictproba_svm = clf_svm.decision_function(X_test.iloc[:,:])
+SVM_Accuracy = accuracy_score(y_test,predictions_svm)
+print("SVM accuracy is ",SVM_Accuracy)
+plotAUC(y_test,predictproba_svm, 'SVM')
+plotAUC(y_test,rfPredictproba, 'Random Forest')
+plotAUC(y_test,LR_Predict,'Logistic Regression')
+plt.show()
+plt.figure(figsize=(6,6))
+plot_confusion_matrix(predictions_svm, normalize=True)
+plt.show()
+
+#K Nearest Neighbors(KNN) with Grid Search CV
+from sklearn.grid_search import GridSearchCV    ## takes too much time to run this cell.
+clf_knn = KNeighborsClassifier()
+k_range = list(range(35, 50))
+param_grid = dict(n_neighbors=k_range)
+grid = GridSearchCV(clf_knn, param_grid, cv=10, scoring='accuracy')
+grid.fit(data_clean.iloc[:,:-1].values, data_clean.iloc[:,-1].values)
+grid_mean_scores = [result.mean_validation_score for result in grid.grid_scores_]# create a list of the mean scores only
+print(grid.best_params_)
+print("---------------")
+print(grid.best_estimator_)
+#outputs the best parameters i.e best number of neighbors
+
+#accuracy, ROC Curve, Confusion matrix
+print("",grid.best_params_['n_neighbors'])
+clf_knn_final = KNeighborsClassifier(n_neighbors=grid.best_params_['n_neighbors'])   #taking the the best from the above cell and using it to find predictions
+clf_knn_final.fit(X_train,y_train)
+knn_pred = clf_knn_final.predict(X_test)
+knn_predictproba = clf_knn_final.predict_proba(X_test)[:,1]
+KNN_Acc = accuracy_score(y_test,knn_pred)
+print("KNN accuracy is ",KNN_Acc)
+plotAUC(y_test,rfPredictproba, 'Random Forest')
+plotAUC(y_test,LR_Predict,'Logistic Regression')
+plotAUC(y_test,predictproba_svm, 'SVM')
+plotAUC(y_test,knn_predictproba,'K Nearest Neighbors')
+plt.show()
+plt.figure(figsize=(6,6))
+plot_confusion_matrix(knn_pred, normalize=True)
+plt.show()
+
+#Bagging
+from sklearn import model_selection
+from sklearn.ensemble import BaggingClassifier
+seed = 7
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+num_trees = 100
+model = BaggingClassifier(base_estimator=randomForest, n_estimators=num_trees, random_state=seed)
+results = model_selection.cross_val_score(model, data_clean.iloc[:,:-1].values, data_clean.iloc[:,-1].values, cv=kfold)
+print(results.mean())
+
+from sklearn import model_selection
+from sklearn.ensemble import BaggingClassifier
+seed = 7
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#num_trees = 100
+model = BaggingClassifier(base_estimator=clf_LR, random_state=seed)
+results = model_selection.cross_val_score(model, data_clean.iloc[:,:-1].values, data_clean.iloc[:,-1].values, cv=kfold)
+print(results.mean())
+
+#Ada Boost classifier
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import load_iris
+from sklearn.ensemble import AdaBoostClassifier
+Ada_clf = AdaBoostClassifier(n_estimators=50)
+scores = cross_val_score(Ada_clf, data_clean.iloc[:,:-1].values, data_clean.iloc[:,-1].values)
+scores.mean()
+
+#Multi-Layer Perceptron Classifier
+from sklearn.neural_network import MLPClassifier
+clf_NN = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+clf_NN.fit(X_train,y_train)     
+predict_NN = clf_NN.predict(X_test)
+predictproba_NN = clf_NN.predict_proba(X_test)[:,1]
+NNAccuracy = accuracy_score(y_test,predict_NN)
+print(NNAccuracy)
+plotAUC(y_test,rfPredictproba, 'Random Forest')
+plotAUC(y_test,LR_Predict,'Logistic Regression')
+plotAUC(y_test,predictproba_svm, 'SVM')
+plotAUC(y_test,knn_predictproba,'K Nearest Neighbors')
+plotAUC(y_test,predictproba_NN,'MLP')
+plt.show()
+plt.figure(figsize=(6,6))
+plot_confusion_matrix(predict_NN, normalize=True)
+plt.show()
+
+#Precision,recall,F1score for all models
+from sklearn.metrics import classification_report
+print("RF",classification_report(y_test, rfPredict, target_names=None))
+print("SVM",classification_report(y_test, predictions_svm, target_names=None))
+print("LR",classification_report(y_test, LR_Predict_bin, target_names=None))
+print("KNN",classification_report(y_test, knn_pred, target_names=None))
+print("MLP",classification_report(y_test, predict_NN, target_names=None))
